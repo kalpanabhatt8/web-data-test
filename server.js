148622f5006356a90bfbb1b205671c3e07ce2d4e
Kalpana Bhatt <kalpanabhatt818@gmail.com>
2026-08-14 10:32:35 +0530

    Add search endpoint to server

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ app.get("/", (req, res) => {
   });
 });
 
+app.get("/search", (req, res) => {
+  const query = req.query.q;
+
+  res.json({
+    query,
+    message: "Search endpoint is working",
+    results: []
+  });
+});
+
 const port = process.env.PORT || 10000;
 
 app.listen(port, "0.0.0.0", () => {
